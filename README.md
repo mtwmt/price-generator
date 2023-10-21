f6be34c824dba809d0a2509f42a1274487918c92
Mandy <mtwmt@hotmail.com>
2023-10-22 04:00:19 +0800

    refactor js => angular

--- README.md
+++ README.md
@@ -3,19 +3,16 @@
 - 20220408 手機也能匯出 PDF 囉。
 - 20220405 加上了呈現客戶端 LOGO，也新增報價、有效日期
 - 20220401 調整了排版，也一併新增匯出圖片功能。
+- 20231022 因 node 18 不再支援 gulp，將網站改用 Angular 翻寫，且新增了稅率計算
 
 ## 使用技術
 
-- javascript ES6
-- Gulp 4.0
-  用來編譯 html、js 以及部署到 github
+- Angular
 
 ## 套件
 
-- [validate](http://validatejs.org/) 驗証
+- ng-bootstrap
 - [tabler](https://tabler.io/) 一個架構在 bootstrap 的框架
-
-- 送出後列印 PDF 檔
 
 ---
 
---
+++ src/app/quotation-model/quotation-model.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+@Component({
+  selector: 'app-quotation-model',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quotation-model.component.html',
+})
+export class QuotationModelComponent implements OnInit {
+  @Input() data!: any;
+  @Input() logo!: any;
+  @Input() isPreview: boolean = false;
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit() {}
+
+  onExportImage() {
+    console.log('onExportImage');
+
+    const preview = document.getElementById('contentToConvert') as HTMLElement;
+    const fileName = ''.concat(
+      new Date().toLocaleString('roc', { hour12: false }),
+      '_quotation.jpg'
+    );
+
+    html2canvas(preview).then(function (canvas) {
+      const img = new Image();
+      img.src = canvas.toDataURL('image/jpeg');
+
+      const link = document.createElement('a');
+      link.href = img.src;
+      link.download = fileName;
+      link.click();
+    });
+  }
+
+  onExportPdf() {
+    console.log('onExportPdf');
+    const preview = document.getElementById('contentToConvert') as HTMLElement;
+    // const doc = new jsPDF();
+    const fileName = ''.concat(
+      new Date().toLocaleString('roc', { hour12: false }),
+      '_quotation.pdf'
+    );
+
+    html2canvas(preview).then(function (canvas) {
+      // Few necessary setting options
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save(fileName); // Generated PDF
+    });
+
+  }
+}
