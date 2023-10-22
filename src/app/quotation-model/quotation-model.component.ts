7d7f92ae3101c13cf0105935ec5a99ead4ae0336
Mandy <mtwmt@hotmail.com>
2023-10-22 14:46:32 +0800

    feat: upate page

--- src/app/quotation-model/quotation-model.component.ts
+++ src/app/quotation-model/quotation-model.component.ts
@@ -15,52 +15,59 @@ export class QuotationModelComponent implements OnInit {
   @Input() logo!: any;
   @Input() isPreview: boolean = false;
 
+  isPrint = false;
   constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit() {}
 
   onExportImage() {
-    console.log('onExportImage');
-
+    this.isPrint = true;
     const preview = document.getElementById('contentToConvert') as HTMLElement;
+
     const fileName = ''.concat(
       new Date().toLocaleString('roc', { hour12: false }),
       '_quotation.jpg'
     );
 
-    html2canvas(preview).then(function (canvas) {
-      const img = new Image();
-      img.src = canvas.toDataURL('image/jpeg');
+    html2canvas(preview)
+      .then(function (canvas) {
+        const img = new Image();
+        img.src = canvas.toDataURL('image/jpeg');
 
-      const link = document.createElement('a');
-      link.href = img.src;
-      link.download = fileName;
-      link.click();
-    });
+        const link = document.createElement('a');
+        link.href = img.src;
+        link.download = fileName;
+        link.click();
+      })
+      .then(() => {
+        this.isPrint = false;
+      });
   }
 
   onExportPdf() {
-    console.log('onExportPdf');
+    this.isPrint = true;
     const preview = document.getElementById('contentToConvert') as HTMLElement;
-    // const doc = new jsPDF();
     const fileName = ''.concat(
       new Date().toLocaleString('roc', { hour12: false }),
       '_quotation.pdf'
     );
 
-    html2canvas(preview).then(function (canvas) {
-      // Few necessary setting options
-      const imgWidth = 208;
-      const pageHeight = 295;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      const heightLeft = imgHeight;
+    html2canvas(preview)
+      .then(function (canvas) {
+        // Few necessary setting options
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png');
-      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
-      const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save(fileName); // Generated PDF
-    });
-
+        const contentDataURL = canvas.toDataURL('image/png');
+        let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+        const position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save(fileName); // Generated PDF
+      })
+      .then(() => {
+        this.isPrint = false;
+      });
   }
 }
