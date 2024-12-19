dacd9603a18abe12133df1861f064e3ccaa7b91f
Mandy <mtwmt@hotmail.com>
2024-12-20 00:52:46 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { PriceGeneratorComponent } from './price-generator/price-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PriceGeneratorComponent],
+  imports: [CommonModule,  PriceGeneratorComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
