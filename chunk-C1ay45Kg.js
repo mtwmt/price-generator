540e19c6f8138857638779393dd1c11589ad1501
Mandy <mtwmt@hotmail.com>
2026-09-14 19:19:09 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/mtwmt/price-generator/commit/0c70fa785d7994cbc7196d544231b9a381d8236b

---
+++ chunk-C1ay45Kg.js
@@ -0,0 +1,1 @@
+import"./chunk-BaY_UQ_z.js";import{q as So}from"./main-ZXGODQYH.js";export{So as default};
--- chunk-CQ66vksQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-BaY_UQ_z.js";import{q as So}from"./main-7TF7ZUZ5.js";export{So as default};
